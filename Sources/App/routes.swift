776afac2a66b0c8da50127a6fc75115ada9c0d02
ShoOshiro <shota.0615.soccer@gmail.com>
2019-04-13 12:32:23 +0900

    aaaa

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,10 +1,16 @@
 import Vapor
 
+
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
     // Basic "It works" example
     router.get { req in
-        return "It works!"
+        
+        return "りょうさん！"
+        
+        
+        
+        
     }
     
     // Basic "Hello, world!" example
